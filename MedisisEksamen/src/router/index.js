9cfc07f9c424d37f42a4da63ffa8960bb95617f5
AlexMoldt <alexmoldt@outlook.com>
2025-01-08 17:01:27 +0100

    hash

--- MedisisEksamen/src/router/index.js
+++ MedisisEksamen/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Forside from "../views/Forside.vue";
 import Produkter from "../views/Produkter.vue";
 import OmMedisis from "../views/OmMedisis.vue";
